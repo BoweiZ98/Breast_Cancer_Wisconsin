7178b8b7f490dfb05f657318f5b392df33d766bc
BoweiZ98 <zbwzhang98@gmail.com>
2023-12-01 19:15:32 -0800

    add random forest

---
+++ Random_Forest.R
@@ -0,0 +1,32 @@
+###### Setting Up ######
+Sys.setLanguage(lang = "en")
+library(MASS)
+library(class)
+library(tidyverse)
+library(caret)
+library(car)
+library(tree)
+library(randomForest)
+
+train = read.csv("datasets/train.csv")
+val = read.csv("datasets/val.csv")
+xnams = dput(colnames(train)[colnames(train) != "Class"])
+
+###### Random Forest ######
+fit.rf = randomForest(Class ~ .,
+                  data = train,
+                  mtry = 3,
+                  importance = TRUE)
+fit.rf
+
+###### Apply to Validation ######
+rf.pred = if_else(
+  predict(fit.rf, newdata = val, type = "response") >= 0.5,
+  1,
+  0
+)
+
+###### Confusion Matrix ######
+rf_conf_matrix = confusionMatrix(data = as.factor(rf.pred),
+                                  reference = as.factor(val[,"Class"]))
+print(rf_conf_matrix) # Ridge:0.9643(0.9186, 0.9883)
